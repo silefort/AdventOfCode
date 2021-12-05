5321baca700f9acec48f85611329de5d2e44b443
Simon Lefort <simon.lefort@gmail.com>
2021-12-05 20:23:15 +0100

    add Day05_02

---
+++ 2021/Day_05_02/main.py
@@ -0,0 +1,117 @@
+import sys 
+sys.path.append('..')
+import tools.fileManipulation as fileManipulation
+
+def compute_cells_to_update(vent):
+    updateList = []
+    if is_vert_line(vent):
+        minRange = min(vent[0][0],(vent[1][0]))
+        maxRange = max(vent[0][0],(vent[1][0]))
+        for i in range(minRange, maxRange+1):
+            updateList.append((i,vent[1][1]))
+    if is_horiz_line(vent):
+        minRange = min(vent[0][1],(vent[1][1]))
+        maxRange = max(vent[0][1],(vent[1][1]))
+        for i in range(minRange, maxRange+1):
+            updateList.append((vent[1][0],i))
+    if is_diag_line(vent):
+        if vent[0][0] < vent[1][0]:
+            x1 = vent[0][0]
+            y1 = vent[0][1]
+            x2 = vent[1][0]
+            y2 = vent[1][1]
+        else:
+            x1 = vent[1][0]
+            y1 = vent[1][1]
+            x2 = vent[0][0]
+            y2 = vent[0][1]
+
+        direction = -1
+        if y1 < y2:
+            direction = 1
+
+        j = 0
+        for i in range(x1, x2+1):
+            updateList.append((i,y1+(direction*j)))
+            j += 1
+
+    return updateList
+
+def string_to_vent(strVent):
+    source = (int(strVent.split(' -> ')[0].split(',')[0]),int(strVent.split(' -> ')[0].split(',')[1]))
+    destination = (int(strVent.split(' -> ')[1].split(',')[0]),int(strVent.split(' -> ')[1].split(',')[1]))
+    return (source, destination)
+
+def is_vert_line(vent):
+    return vent[0][1] == vent[1][1]
+
+def is_horiz_line(vent):
+    return vent[0][0] == vent[1][0]
+
+def is_diag_line(vent):
+    return (abs(vent[0][0]-vent[1][0])) == (abs(vent[0][1]-vent[1][1]))
+    # return ((vent[0][0] == vent[0][1]) and (vent[1][0] == vent[1][1])) or ((vent[0][0] == vent[1][1]) and (vent[1][0] == vent[0][1]))
+
+def get_vents(filename):
+
+    lines = fileManipulation.read_file_line_by_line(filename)
+    vents = []
+    for l in lines:
+        vents.append(string_to_vent(l))
+
+    return vents
+
+def get_max_x(vents):
+
+    xList = []
+    for v in vents:
+        xList.append(v[0][0])
+        xList.append(v[1][0])
+
+    return max(xList)
+
+def get_max_y(vents):
+
+    yList = []
+    for v in vents:
+        yList.append(v[0][1])
+        yList.append(v[1][1])
+
+    return max(yList)
+
+def update_matrix(vents, matrix):
+
+    currentMatrix = matrix
+    for v in vents:
+        cellsToUpdate = compute_cells_to_update(v)
+        for c in cellsToUpdate:
+            y = c[0]
+            x = c[1]
+            currentMatrix[x][y] = currentMatrix[x][y] + 1
+
+    return currentMatrix
+
+def main(fichier):
+
+    vents = get_vents(fichier)
+    rows = get_max_x(vents) + 1
+    columns = get_max_y(vents) + 1
+    matrix = []
+    for r in range(0,rows):
+        currentRow = []
+        for c in range(0,columns):
+            currentRow.append(0)
+        matrix.append(currentRow)
+
+    matrix = update_matrix(vents,matrix)
+
+    numberOfDangerousAreas = 0
+    for r in range(0,rows):
+        for c in range(0, columns):
+            if matrix[r][c] >= 2:
+                numberOfDangerousAreas += 1
+
+    return numberOfDangerousAreas
+
+if __name__ == "__main__":
+       print(main('input.txt'))
---
+++ 2021/Day_05_02/test_code.py
@@ -0,0 +1,231 @@
+from main import *
+
+def test_update_matrix_1():
+    # Given
+    # vents = [((0,9),(5,9)),
+            # ((5,5),(8,2))]
+    vents = [((0,9),(5,9)),
+            ((0,0), (9,9))]
+
+    rows = get_max_x(vents) + 1
+    columns = get_max_y(vents) + 1
+    matrix = []
+    for r in range(0,rows):
+        currentRow = []
+        for c in range(0,columns):
+            currentRow.append(0)
+        matrix.append(currentRow)
+
+    # When
+    matrix = update_matrix(vents, matrix)
+
+    # Then
+    assert matrix ==  [[1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                       [1, 1, 1, 1, 1, 1, 0, 0, 0, 1]]
+
+def test_update_matrix_2():
+    # Given
+    # vents = [((0,9),(5,9)),
+            # ((5,5),(8,2))]
+    vents = [((0,9),(5,9)),
+            ((0,0), (9,9)),
+            ((5,3), (3,5))]
+
+    rows = get_max_x(vents) + 1
+    columns = get_max_y(vents) + 1
+    matrix = []
+    for r in range(0,rows):
+        currentRow = []
+        for c in range(0,columns):
+            currentRow.append(0)
+        matrix.append(currentRow)
+
+    # When
+    matrix = update_matrix(vents, matrix)
+
+    # Then
+    assert matrix ==  [[1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 2, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                       [1, 1, 1, 1, 1, 1, 0, 0, 0, 1]]
+
+def test_main():
+    # Given
+    fichier = 'test_input.txt'
+
+    # When
+    resultat = main(fichier)
+
+    # Then
+    assert resultat == 12
+
+def test_string_to_vent():
+    # Given
+    strVent = "0,9 -> 5,9"
+
+    # When
+    resultat = string_to_vent(strVent)
+
+    # Then
+    assert resultat == ((0,9),(5,9))
+
+def test_get_vents():
+    # Given
+    fichier = 'test_input.txt'
+
+    # When
+    resultat = get_vents(fichier)
+
+    # Then
+    assert resultat == [((0,9),(5,9)),
+                        ((8,0),(0,8)),
+                        ((9,4),(3,4)),
+                        ((2,2),(2,1)),
+                        ((7,0),(7,4)),
+                        ((6,4),(2,0)),
+                        ((0,9),(2,9)),
+                        ((3,4),(1,4)),
+                        ((0,0),(8,8)),
+                        ((5,5),(8,2))]
+
+def test_get_max_x():
+    # Given
+    vents = [((0,9),(5,9)),
+            ((8,0),(0,8)),
+            ((9,4),(3,4)),
+            ((2,2),(2,1)),
+            ((7,0),(7,4)),
+            ((6,4),(2,0)),
+            ((0,9),(2,9)),
+            ((3,4),(1,4)),
+            ((0,0),(8,8)),
+            ((5,5),(8,2))]
+
+    # When
+    resultat = get_max_x(vents)
+
+    # Then
+    assert resultat == 9
+
+def test_get_max_y():
+    # Given
+    vents = [((0,9),(5,9)),
+            ((8,0),(0,8)),
+            ((9,4),(3,4)),
+            ((2,2),(2,1)),
+            ((7,0),(7,4)),
+            ((6,4),(2,0)),
+            ((0,9),(2,9)),
+            ((3,4),(1,4)),
+            ((0,0),(8,8)),
+            ((5,5),(8,2))]
+
+    # When
+    resultat = get_max_x(vents)
+
+    # Then
+    assert resultat == 9
+
+def test_is_vert_line():
+    # Given
+    line =  ((0,9),(5,9))
+
+    # When
+    resultat = is_vert_line(line)
+
+    # Then
+    assert resultat == True
+
+def test_is_not_vert_line():
+    # Given
+    line =  ((0,4),(5,9))
+
+    # When
+    resultat = is_vert_line(line)
+
+    # Then
+    assert resultat == False
+
+def test_is_horiz_line():
+    # Given
+    line =  ((2,9),(2,4))
+
+    # When
+    resultat = is_horiz_line(line)
+
+    # Then
+    assert resultat == True
+
+def test_is_not_horiz_line():
+    # Given
+    line =  ((0,4),(5,9))
+
+    # When
+    resultat = is_horiz_line(line)
+
+    # Then
+    assert resultat == False
+
+def test_is_diag_line_1():
+    # Given
+    line =  ((1,1),(3,3))
+
+    # When
+    resultat = is_diag_line(line)
+
+    # Then
+    assert resultat == True
+
+def test_is_diag_line_2():
+    # Given
+    line =  ((9,7),(7,9))
+
+    # When
+    resultat = is_diag_line(line)
+
+    # Then
+    assert resultat == True
+
+def test_is_not_diag_line():
+    # Given
+    line =  ((8,7),(7,9))
+
+    # When
+    resultat = is_diag_line(line)
+
+    # Then
+    assert resultat == False
+
+def test_compute_cells_to_update_vert():
+    # Given
+    line =  ((0,9),(5,9))
+
+    # When
+    resultat = compute_cells_to_update(line)
+
+    # Then
+    assert resultat == [(0,9),(1,9),(2,9),(3,9),(4,9),(5,9)]
+
+def test_compute_cells_to_update_horiz():
+    # Given
+    line =  ((4,9),(4,6))
+
+    # When
+    resultat = compute_cells_to_update(line)
+
+    # Then
+    assert resultat == [(4,6),(4,7),(4,8),(4,9)]
